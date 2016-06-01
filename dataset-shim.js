a6ff513d963354c15c35221fa079a502ecfc9c25
sfger <sfger111@163.com>
2016-06-01 20:21:49 +0800

    update

---
+++ dataset-shim.js
@@ -0,0 +1,18 @@
+(function(HTMLElement){
+	if(!('dataset' in HTMLElement.prototype)) return;
+	Object.defineProperty(HTMLElement.prototype, 'dataset', {
+		get: function(){
+			var ret = {};
+			var attributes = this.attributes;
+			for(var i=0,il=attributes.length; i<il; i++){
+				var name = attributes[i].name;
+				var _name = name.replace(/^data-/, '');
+				if(name!==_name){
+					_name = _name.replace(/-([\w])/g, function($0, $1){ return $1.toUpperCase(); });
+					ret[_name] = attributes[i].value;
+				}
+			}
+			return ret;
+		}
+	});
+})(window.HTMLElement||window.Element);
