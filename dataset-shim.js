2ba0de43e33b2d060ca29ab89996affdd38c740f
sfger <sfger111@163.com>
2016-06-16 19:35:41 +0800

    add dataset setter

--- dataset-shim.js
+++ dataset-shim.js
@@ -1,18 +1,33 @@
 (function(HTMLElement){
 	if('dataset' in HTMLElement.prototype) return;
-	Object.defineProperty(HTMLElement.prototype, 'dataset', {
+	var camelize = function(s){
+		return s.replace(/-([a-zA-Z])/g, function($0, $1){
+			return $1.toUpperCase();
+		});
+	};
+	var ie8 = /MSIE 8/.test(navigator.userAgent);
+	var descriptor = {
 		get: function(){
-			var ret = {};
+			var ret = ie8 ? document.createElement('DOMStringMap') : {};
 			var attributes = this.attributes;
 			for(var i=0,il=attributes.length; i<il; i++){
 				var name = attributes[i].name;
 				var _name = name.replace(/^data-/, '');
-				if(name!==_name){
-					_name = _name.replace(/-([a-zA-Z])/g, function($0, $1){ return $1.toUpperCase(); });
-					ret[_name] = attributes[i].value;
-				}
+				if(name===_name) continue;
+				var dataset_item_descriptor = (function(dom, dom_attr_name, dataset_item_val){
+					return {
+						get: function(){
+							return dataset_item_val;
+						},
+						set: function(value){
+							return dom.setAttribute(dom_attr_name, value);
+						}
+					};
+				})(this, name, attributes[i].value);
+				Object.defineProperty(ret, camelize(_name), dataset_item_descriptor);
 			}
 			return ret;
 		}
-	});
+	};
+	Object.defineProperty(HTMLElement.prototype, 'dataset', descriptor);
 })(window.HTMLElement||window.Element);
